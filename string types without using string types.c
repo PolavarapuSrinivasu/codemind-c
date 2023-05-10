51e0c12c15c9989387e75c090a91af4b49014d64
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2023-05-10 18:51:07 +0530

    Add files via upload

---
+++ string types without using string types.c
@@ -0,0 +1,61 @@
+//String length without using strlen()//
+//#include<stdio.h>
+//int main()
+//{
+//	char str[100];
+//	int len=0,i;
+//	scanf("%[^\n]s",str);
+//	for(i=0;str[i]!='\0';i++)
+//	{
+//		len++;
+//	}
+//	printf("%d",len);
+//}
+
+//String copy without using strcpy()//
+//#include<stdio.h>
+//int main()
+//{
+//	char str1[100],str2[100];
+///	int i;
+//	scanf("%[^\n]s",str1);
+//	for(i=0;str1[i]!='\0';i++)
+//	{
+//		str2[i]=str1[i];
+//	}
+//	printf("%s",str2);
+//}
+
+//String uppercase without using strupr()//
+//#include<stdio.h>
+//int main()
+//{
+//	char str[100];
+//	int i;
+//	scanf("%[^\n]s",str);
+///	for(i=0;str[i]!='\0';i++)
+//	{
+//		if(str[i]>=97 && str[i]<=122)
+//		{
+//		str[i]=str[i]-32;
+//	    }
+//	}
+//	printf("%s",str);
+//}
+
+/*String lowercase without using strlwr()
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i;
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>=65 && str[i]<=90)
+		{
+		str[i]=str[i]+32;
+	    }
+	}
+	printf("%s",str);
+}*/
