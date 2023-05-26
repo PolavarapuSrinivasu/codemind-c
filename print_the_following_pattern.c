d1be3eea82dfee3b7e1ad4af70c74310ebae212e
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2023-05-26 12:15:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,14 +3,12 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int as=64+n;
-    for(int i=n;i>=1;i--)
+    for(int i=1;i<=n;i++)
     {
-        for(int j=i;j>=1;j--)
+        for(int j=1;j<=i;j++)
         {
-            printf("%c ",as);
+            printf("%d",j);
         }
-        as-=1;
         printf("
 ");
     }
